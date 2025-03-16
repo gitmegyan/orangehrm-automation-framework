97807499418c2e13066348ad3db4b7043a2d727d
gitmegyan <gengyana@gmail.com>
2025-03-16 12:43:14 +0530

    Changes

---
+++ src/main/java/com/hrm/config/RemoteWebdriverConfig.java
@@ -0,0 +1,53 @@
+package com.hrm.config;
+
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
+
+import java.net.URL;
+import java.time.Duration;
+
+@Lazy
+@Configuration
+@Profile("remote")
+public class RemoteWebdriverConfig {
+
+    @Value("${default.timeout:200}")
+    private int timeout;
+
+    @Value("${seelnium.grid.url}")
+    private URL url;
+
+    @Bean
+    @ConditionalOnProperty(name="browser"  , havingValue = "firefox")
+    public WebDriver remoteFirefoxWebDirver(){
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("firefox");
+        // capabilities.setPlatform(Platform.WINDOWS);
+        return new RemoteWebDriver(this.url, capabilities);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public WebDriver remoteChromeWebDirver(){
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+        // capabilities.setPlatform(Platform.WINDOWS);
+        return new RemoteWebDriver(this.url, capabilities);
+    }
+
+    @Bean
+    public WebDriverWait webDriverWait(WebDriver driver) {
+        return new WebDriverWait(driver, Duration.ofSeconds(this.timeout));
+    }
+}
--- src/main/java/com/hrm/config/WebDriverConfig.java
+++ src/main/java/com/hrm/config/WebDriverConfig.java
@@ -5,27 +5,31 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.*;
 
 import java.time.Duration;
 
+@Lazy
 @Configuration
+@Profile("!remote")
 public class WebDriverConfig {
 
     @Value("${default.timeout:200}")
     private int timeout;
 
-    @Primary
     @Bean
-    public WebDriver chromeDriver() {
-        return new ChromeDriver();
+    @ConditionalOnProperty(name = "browser", havingValue = "firefox")
+    public WebDriver firefoxDriver() {
+        return new FirefoxDriver();
     }
 
     @Bean
-    public WebDriver firefoxDriver() {
-        return new FirefoxDriver();
+    @ConditionalOnMissingBean
+    public WebDriver chromeDriver() {
+        return new ChromeDriver();
     }
 
     @Bean
